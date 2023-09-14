4f2ae2d8c2dbf4fd2f16082fa62c51e2479e6358
Topher <topher.dan@gmail.com>
2023-09-14 12:58:15 -0700

    remove forced https since we're terminating SSL at nginx

--- server.js
+++ server.js
@@ -37,19 +37,6 @@ app.disable("x-powered-by");
 //force HTTPS in production
 if (process.env.ENVIRONMENT === "production") {
   app.set("trust proxy", ["127.0.0.1", "10.0.0.0/8"]);
-
-  app.use(({ secure, hostname, url, port }, response, next) => {
-    if (!secure) {
-      return response.redirect(
-        308,
-        `https://${hostname}${url}${port ? `:${port}` : ""}`
-      );
-    }
-
-    next();
-  });
-} else {
-  console.log("ENVIRONMENT is not 'production', HTTPS not forced");
 }
 
 const hbs = create({
